879d0f1047c7a53a9a0efbfe32c0ef48d661e799
Kevin <kevinrsa@gmail.com>
2018-08-31 07:24:09 +0100

    Add Fx Weekly and Monthly tests

--- test/ThreeFourteen.AlphaVantage.Test/Builders/Fx/FxMonthlyBuilderTests.cs
+++ test/ThreeFourteen.AlphaVantage.Test/Builders/Fx/FxMonthlyBuilderTests.cs
@@ -6,7 +6,7 @@ namespace ThreeFourteen.AlphaVantage.Test.Builders.Fx
 {
     public class FxMonthlyBuilderTests : BuilderTestsBase
     {
-        [Fact(Skip = "Pending data")]
+        [Fact]
         public async void Get_ShouldReturnValidData()
         {
             var result = await AlphaVantage.Fx.Monthly("EUR", "USD")
@@ -17,25 +17,25 @@ namespace ThreeFourteen.AlphaVantage.Test.Builders.Fx
             ServiceMock.LatestParameters[ParameterFields.Function].ShouldBe(Function.Fx.Monthly.Value);
 
             result.Meta.Count.ShouldBe(5);
-            result.Meta["Information"].ShouldBe("Forex Daily Prices (open, high, low, close)");
+            result.Meta["Information"].ShouldBe("Forex Monthly Prices (open, high, low, close)");
             result.Meta["From Symbol"].ShouldBe("EUR");
             result.Meta["To Symbol"].ShouldBe("USD");
-            result.Meta["Last Refreshed"].ShouldBe("2018-08-22 14:15:00");
+            result.Meta["Last Refreshed"].ShouldBe("2018-08-31 14:10:00");
             result.Meta["Time Zone"].ShouldBe("GMT+8");
 
-            result.Data.Length.ShouldBe(101);
+            result.Data.Length.ShouldBe(220);
             var first = result.Data.First();
-            first.Timestamp.ShouldBe(Formats.ParseDateTime("2018-08-22"));
-            first.Open.ShouldBe(1.1533);
-            first.High.ShouldBe(1.1601);
-            first.Low.ShouldBe(1.1526);
-            first.Close.ShouldBe(1.1571);
+            first.Timestamp.ShouldBe(Formats.ParseDateTime("2018-08-31"));
+            first.Open.ShouldBe(1.1686);
+            first.High.ShouldBe(1.1733);
+            first.Low.ShouldBe(1.1300);
+            first.Close.ShouldBe(1.1677);
             var last = result.Data.Last();
-            last.Timestamp.ShouldBe(Formats.ParseDateTime("2018-04-27"));
-            last.Open.ShouldBe(1.2104);
-            last.High.ShouldBe(1.2133);
-            last.Low.ShouldBe(1.2053);
-            last.Close.ShouldBe(1.2125);
+            last.Timestamp.ShouldBe(Formats.ParseDateTime("2000-05-31"));
+            last.Open.ShouldBe(0.9115);
+            last.High.ShouldBe(0.9411);
+            last.Low.ShouldBe(0.8842);
+            last.Close.ShouldBe(0.9357);
         }
     }
 }
--- test/ThreeFourteen.AlphaVantage.Test/Builders/Fx/FxWeeklyBuilderTests.cs
+++ test/ThreeFourteen.AlphaVantage.Test/Builders/Fx/FxWeeklyBuilderTests.cs
@@ -6,7 +6,7 @@ namespace ThreeFourteen.AlphaVantage.Test.Builders.Fx
 {
     public class FxWeeklyBuilderTests : BuilderTestsBase
     {
-        [Fact(Skip = "Pending data")]
+        [Fact]
         public async void Get_ShouldReturnValidData()
         {
             var result = await AlphaVantage.Fx.Weekly("EUR", "USD")
@@ -17,25 +17,25 @@ namespace ThreeFourteen.AlphaVantage.Test.Builders.Fx
             ServiceMock.LatestParameters[ParameterFields.Function].ShouldBe(Function.Fx.Weekly.Value);
 
             result.Meta.Count.ShouldBe(5);
-            result.Meta["Information"].ShouldBe("Forex Daily Prices (open, high, low, close)");
+            result.Meta["Information"].ShouldBe("Forex Weekly Prices (open, high, low, close)");
             result.Meta["From Symbol"].ShouldBe("EUR");
             result.Meta["To Symbol"].ShouldBe("USD");
-            result.Meta["Last Refreshed"].ShouldBe("2018-08-22 14:15:00");
+            result.Meta["Last Refreshed"].ShouldBe("2018-08-31 14:10:00");
             result.Meta["Time Zone"].ShouldBe("GMT+8");
 
-            result.Data.Length.ShouldBe(101);
+            result.Data.Length.ShouldBe(958);
             var first = result.Data.First();
-            first.Timestamp.ShouldBe(Formats.ParseDateTime("2018-08-22"));
-            first.Open.ShouldBe(1.1533);
-            first.High.ShouldBe(1.1601);
-            first.Low.ShouldBe(1.1526);
-            first.Close.ShouldBe(1.1571);
+            first.Timestamp.ShouldBe(Formats.ParseDateTime("2018-08-31"));
+            first.Open.ShouldBe(1.1628);
+            first.High.ShouldBe(1.1733);
+            first.Low.ShouldBe(1.1593);
+            first.Close.ShouldBe(1.1677);
             var last = result.Data.Last();
-            last.Timestamp.ShouldBe(Formats.ParseDateTime("2018-04-27"));
-            last.Open.ShouldBe(1.2104);
-            last.High.ShouldBe(1.2133);
-            last.Low.ShouldBe(1.2053);
-            last.Close.ShouldBe(1.2125);
+            last.Timestamp.ShouldBe(Formats.ParseDateTime("2000-04-28"));
+            last.Open.ShouldBe(0.9374);
+            last.High.ShouldBe(0.9450);
+            last.Low.ShouldBe(0.9026);
+            last.Close.ShouldBe(0.9090);
         }
     }
 }
