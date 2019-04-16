4d42847442f731ffb95d42ea2bb363394c1ad13c
Ivan Perez <irperez@gmail.com>
2019-04-15 21:41:12 -0400

    Added a new exception to take action when we hit the API limits.

---
+++ src/ThreeFourteen.AlphaVantage/AlphaVantageApiLimitException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ThreeFourteen.AlphaVantage
+{
+    public class AlphaVantageApiLimitException : AlphaVantageException
+    {
+        public AlphaVantageApiLimitException(string message, IReadOnlyDictionary<string, string> fields)
+            : base(message, fields)
+        {
+
+        }
+
+        public AlphaVantageApiLimitException(string message, IReadOnlyDictionary<string, string> fields, Exception innerException)
+            : base(message, fields, innerException)
+        {
+
+        }
+    }
+}
